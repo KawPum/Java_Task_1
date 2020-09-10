04a5aca43a4e0f0cbd12cb1b059fa5b6427a0ce1
KaPu <eremey.milovanov.msc@gmail.com>
2020-09-11 00:29:31 +0300

    Completion of all tasks 2.0
    Did subtask #3

--- task_1_2/src/com/company/Main.java
+++ task_1_2/src/com/company/Main.java
@@ -5,13 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int[] numbers = new int[10];
+        int sum = 0;
         Scanner sc = new Scanner(System.in);
-        String str;
+        /*String str;
 	    for (;;)
         {
             str = sc.nextLine();
             if (str.isEmpty()) break;
             else System.out.println(str);
+        }*/
+
+	    /*for (int i = 0; i<10;i++)  //for//
+        {
+            numbers[i] = sc.nextInt();
+            sum += numbers[i];
+        }*/
+	    int count = 0;   //while//
+	    /*while (count < numbers.length)
+        {
+            numbers[count] = sc.nextInt();
+            sum += numbers[count];
+            count++;
+        }*/
+	    do {
+            numbers[count] = sc.nextInt();
+            sum += numbers[count];
+            count++;
         }
+	    while (count < numbers.length);
+
+	    System.out.println(sum);
     }
 }
